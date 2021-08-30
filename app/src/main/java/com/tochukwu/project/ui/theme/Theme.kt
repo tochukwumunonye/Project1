138ba9a16c2fd6db7e27d7bd5f7c973e96432857
Tochukwumunonye <tochukwuokeymunonye@gmail.com>
2021-08-30 01:11:27 +0100

    first

---
+++ app/src/main/java/com/tochukwu/project/ui/theme/Theme.kt
@@ -0,0 +1,26 @@
+package com.tochukwu.project.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+
+
+private val DarkColorPalette = darkColors(
+    primary = GreenAccent,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray,
+    surface = MediumGray,
+    onSurface = LightGray
+)
+
+@Composable
+fun ProjectTheme(content: @Composable () -> Unit){
+    MaterialTheme (
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
