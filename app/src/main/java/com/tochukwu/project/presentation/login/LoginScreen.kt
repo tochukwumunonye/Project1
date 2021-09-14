50c1aa83be276999599c9f75a450373bd67370cd
Tochukwumunonye <tochukwuokeymunonye@gmail.com>
2021-09-14 14:54:45 +0100

    first

--- app/src/main/java/com/tochukwu/project/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/tochukwu/project/presentation/login/LoginScreen.kt
@@ -38,7 +38,7 @@ fun LoginScreen(
                     start = SpaceLarge,
                     end = SpaceLarge,
                     top = SpaceLarge,
-                    bottom = 50.dp
+                    bottom = 50.dp /////
                 )
         ) {
             Column(
